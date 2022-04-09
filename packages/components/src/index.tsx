e8152980aaa78629170a845dc3945d75393a3725
David Denton <dpdenton@yahoo.com>
2022-04-09 23:42:31 +0100

    remaining config to get it working on Android - turns out $rootDir isn't the root dir defined in gradle or the --root-dir flag :/
    Also got react-native-web configured in 'web' package

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -8,15 +8,7 @@ import {
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import styled from "@emotion/native";
-
-
 
 const App = () => {
   return (
@@ -26,34 +18,14 @@ const App = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          <Header />
-          <StyledView><Text style={styles.sectionTitle}>Hello WOrld</Text></StyledView>
+          <StyledView><Text style={styles.sectionTitle}>Dont matter</Text></StyledView>
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this hllo
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
         </ScrollView>
       </SafeAreaView>
