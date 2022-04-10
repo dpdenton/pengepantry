2e7023b332bc5dd74bc9acc80e358ba4720cb30c
David Denton <dpdenton@yahoo.com>
2022-04-10 19:11:09 +0100

    add storybook

---
+++ packages/web/src/stories/Button.stories.tsx
@@ -0,0 +1,17 @@
+// Button.stories.js|jsx|ts|tsx
+import App from '@penge-pantry/components'
+const Button = () => <button>Hello</button>
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: 'Button',
+  component: Button,
+};
+
+const Template = (args: any) => ({
+  //👇 Your template goes here
+});
+
+export const Primary = () => <App />
