9158ac505c9db92987a133254e297164fe5db3ba
David Denton <dpdenton@yahoo.com>
2022-06-05 10:16:12 +0100

    fix eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
   plugins: ['@typescript-eslint', 'eslint-plugin-import'],
   overrides: [
     {
@@ -9,7 +16,7 @@ module.exports = {
         'no-undef': 'off',
         'import/no-cycle': 'error',
         '@typescript-eslint/no-shadow': ['error'],
-        '@typescript-eslint/no-unused-vars': ['error'],
+        '@typescript-eslint/no-unused-vars': 0,
       },
     },
   ],
