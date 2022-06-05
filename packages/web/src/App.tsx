2f97ca2f634dd1cfc2e972814e064b56313a2a59
David Denton <dpdenton@yahoo.com>
2022-06-05 10:16:12 +0100

    fix eslint issues

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,6 +1,12 @@
 import './App.css';
+import {NavBar} from 'components/nav-bar/NavBar';
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <NavBar />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
 
 export default App;
---
+++ packages/web/src/components/nav-bar/NavBar.tsx
@@ -0,0 +1,11 @@
+import {Row} from '@pengepantry/core/lib/components/layout/Row';
+
+export const NavBar = () => {
+  return (
+    <nav>
+      <Row>
+        <div>Penge Pantry</div>
+      </Row>
+    </nav>
+  );
+};
