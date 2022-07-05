5485105c49d06ce5d47717ff2d4f241a17725f94
David Denton <dpdenton@yahoo.com>
2022-07-05 19:57:29 +0100

    style tweaks to NavBar and AppSection

--- packages/webapp/components/app-section/AppSection.tsx
+++ packages/webapp/components/app-section/AppSection.tsx
@@ -2,6 +2,8 @@ import React, {PropsWithChildren} from 'react';
 
 export const AppSection: React.FC<PropsWithChildren> = ({children}) => {
   return (
-    <section className="px-5 md:px-20 bg-primary-light">{children}</section>
+    <section className="px-5 md:px-20 py-4 bg-primary-light">
+      {children}
+    </section>
   );
 };
