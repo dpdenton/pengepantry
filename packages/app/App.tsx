1df04c29d31b2f6a622429b5a8db434fdbfac593
David Denton <dpdenton@yahoo.com>
2022-08-24 23:02:05 +0100

    WIP: fix broken react-native-svg on android;
    Fix failure to find emulator - had to add paths to bash_profile: https://stackoverflow.com/questions/38835931/react-native-adb-reverse-enoent

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -4,16 +4,12 @@ import {Provider} from 'react-redux';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {reduxStore} from '@pengepantry/core/lib/store/store';
 import Navigation from 'navigation/RootStack';
-import {themeConfig} from '@pengepantry/core/lib/theme/config';
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <Provider store={reduxStore}>
-        <StatusBar
-          barStyle="dark-content"
-          backgroundColor={themeConfig.theme.backgroundColor.primary}
-        />
+        <StatusBar barStyle="dark-content" />
         <Navigation />
       </Provider>
     </SafeAreaProvider>
